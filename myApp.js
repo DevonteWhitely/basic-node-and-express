f7e95c22a5680ae9e1f05835d66811b39f247867
Devonte Whitely <dwhit85@lsu.edu>
2021-05-29 14:47:14 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -11,21 +11,22 @@ app.get("/", (req, res) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 // app.get("/json", (req, res) => {
-//     res.json({
-//         "message": response
-//     });
+//     if (process.env.MESSAGE_STYLE === "uppercase") {
+//         res.json({
+//             "message": "HELLO JSON"
+//         })
+//     } else {
+//         res.json({
+//             "message": "Hello json"
+//         })
+//     }
 // });
 
 app.get("/json", (req, res) => {
+    var response = res.json({"message": "Hello json"});
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({
-            "message": "HELLO JSON"
-        })
-    } else {
-        res.json({
-            "message": "Hello json"
-        })
-    }
+        response = response.message.toUpperCase();
+    } res.json(response);
 });
 
 
