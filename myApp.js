6cb6502201273aff32e85aff1ddbc46a7ba3134e
Devonte Whitely <dwhit85@lsu.edu>
2021-05-29 16:28:12 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -27,6 +27,15 @@ app.get("/json", (req, res) => {
     }
 });
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.send({
+        time: req.time
+    });
+});
+
 
 
 
